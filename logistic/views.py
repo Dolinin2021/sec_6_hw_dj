86a15f96b996e75fec7646a47dcd8386e3c4b776
Dolinin2021 <programmistdolinin@mail.ru>
2022-03-18 17:25:10 +0600

    Correction

--- logistic/views.py
+++ logistic/views.py
@@ -1,9 +1,16 @@
+import django_filters
 from rest_framework.filters import SearchFilter
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class StockFilter(django_filters.FilterSet):
+    products = django_filters.NumberFilter()
+    class Meta:
+        model = Stock
+        fields = ['products__id']
 
 
 class ProductViewSet(ModelViewSet):
@@ -16,6 +23,4 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    filter_backends = [SearchFilter]
-    search_fields = ['products__id', 'products__title', 'products__description']
-    pagination_class = LimitOffsetPagination
+    filter_class = StockFilter
