a2a01e6fdae037838f12f6aa292ed7437b0cd1e0
Dolinin2021 <programmistdolinin@mail.ru>
2022-03-19 16:23:07 +0600

    Correction

--- logistic/views.py
+++ logistic/views.py
@@ -1,26 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
-
-
-# class StockFilter(django_filters.FilterSet):
-#     products = django_filters.NumberFilter()
-#     class Meta:
-#         model = Stock
-#         fields = ['products__id']
-
-
-class StockSearchFilter(SearchFilter):
-    def filter_queryset(self, request, queryset, view):
-        queryset = super().filter_queryset(request, queryset, view)
-        param = request.query_params.get('products')
-        if param:
-            result = Stock.objects.filter(products=param)
-            print(result)
-        else:
-            return queryset
 
 
 class ProductViewSet(ModelViewSet):
@@ -33,5 +16,5 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    filter_backends = [SearchFilter]
-    filter_class = StockSearchFilter
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products']
